b716922515c88725a71eae7757579f53f0d5ee47
TomLe1227 <2005thomasle@gmail.com>
2026-02-26 13:17:09 -0800

    Intitiated DUT for butterfly and fft4

---
+++ tb_butterfly.sv
@@ -0,0 +1,17 @@
+module tb_butterfly;
+
+localparam int WIDTH = 32;
+localparam int HALF = WIDTH/2;
+logic signed [WIDTH-1:0] A, B, W;
+logic signed [WIDTH-1:0] out0, out1;
+
+// Instantiate the butterfly module (Device Under Test)
+butterfly #(.WIDTH(WIDTH)) dut (
+    .A(A),
+    .B(B),
+    .W(W),
+    .out0(out0),
+    .out1(out1)
+);
+
+endmodule
---
+++ tb_fft4.sv
@@ -0,0 +1,36 @@
+`timescale 1ps/1ps
+
+module tb_fft4;
+
+localparam int WIDTH = 32;
+logic clk;
+logic rst;
+logic start;
+logic done;
+logic signed [WIDTH-1:0] in0, in1, in2, in3;
+logic signed [WIDTH-1:0] out0, out1, out2, out3;
+
+fft4 #(.WIDTH(WIDTH)) dut (
+    .clk(clk),
+    .rst(rst),
+    .start(start),
+    .done(done),
+    .in0(in0),
+    .in1(in1),
+    .in2(in2),
+    .in3(in3),
+    .out0(out0),
+    .out1(out1),
+    .out2(out2),
+    .out3(out3)
+);
+
+initial clk = 0;;
+always #10 clk = ~clk;
+
+initial begin 
+    $display("");
+end
+
+
+endmodule
