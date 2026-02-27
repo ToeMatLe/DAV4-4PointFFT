76da0c306945eadebc964fea6f95c59a563ff2f7
TomLe1227 <2005thomasle@gmail.com>
2026-02-27 15:57:09 -0800

    kms

--- Butterfly/butterfly.sv
+++ Butterfly/butterfly.sv
@@ -7,7 +7,7 @@ module butterfly #(
     output signed [WIDTH-1:0] out0,
     output signed [WIDTH-1:0] out1
 );
-    localparam int HALF = WIDTH/2;
+    localparam intHALF = WIDTH/2;
     logic signed [HALF-1: 0] A_real, B_real, W_real, A_imag, B_imag, W_imag;
     // Split the inputs into half real and half imaginary parts
     assign A_real = A[WIDTH-1:HALF];
@@ -21,16 +21,8 @@ module butterfly #(
     // B*W = (B_real + jB_imag)(W_real + jW_imag) = (B_real*W_real - B_imag*W_imag) + j(B_real*W_imag + B_imag*W_real)
     logic signed [WIDTH-1:0] temp_real, temp_imag;
     assign temp_real = B_real * W_real - B_imag * W_imag;
-    assign temp_imag = B_real * W_imag + B_imag * W_real
+    assign temp_imag = B_real * W_imag + B_imag * W_real;
 
-    logic signed [HALF-1:0] out0_real, out0_imag;
-    logic signed [HALF-1:0] out1_real, out1_imag;
-
-    assign out0_real = A_real + temp_real;
-    assign out0_imag = A_imag + temp_imag;
-    assign out1_real = A_real - temp_real;
-    assign out1_imag = A_imag - temp_imag;
-
-    assign out0 = {out0_real, out0_imag};
-    assign out1 = {out1_real, out1_imag};
+    assign out0 = {A_real + temp_real, A_imag + temp_imag}; // out0 = A + B*W
+    assign out1 = {A_real - temp_real, A_imag - temp_imag}; // out1 = A - B*W
 endmodule
